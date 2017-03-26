51bd656cee9b3516d56577530f1c484364cd0715
Anton Shulgin <anton.shulgin@gmx.com>
2017-03-26 11:25:39 +0300

    more violence

--- snoike.js
+++ snoike.js
@@ -120,7 +120,7 @@
 
 		function resume() {
 			pause();
-			internals.updateInterval = setInterval(updateFrame, 175);
+			internals.updateInterval = setInterval(updateFrame, 150);
 			internals.isPaused = false;
 		}
 
